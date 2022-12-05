fe6c4af01b3b93d929d21f13b93e8228d08b37a6
pemulis0x <bcl4rk@protonmail.com>
2022-12-04 17:00:17 -0700

    formatting

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{self, Error};
+use std::io::{self};
 
 fn fully_contains(pair: Vec<(i32, i32)>) -> i32 {
     let view = &pair[..];
@@ -23,26 +23,22 @@ fn overlaps(pair: Vec<(i32, i32)>) -> i32 {
      } 0
 }
 
-fn line_to_pair(line: Result<String, Error>) -> Vec<(i32, i32)> {
-    let s = line.unwrap();
-    let fmt: Vec<(i32, i32)> = s.split(',').collect::<Vec<_>>().iter()
-        .map(|s| s.split('-')
-        .collect::<Vec<_>>())
+fn line_to_pair(s: String) -> Vec<(i32, i32)> {
+    s.split(',').collect::<Vec<_>>().iter()
+        .map(|s| s.split('-').collect::<Vec<_>>())
         .map(|v| (v[0].parse::<i32>().unwrap(), v[1].parse::<i32>().unwrap()))
-        .collect::<Vec<_>>();
-
-    fmt
+        .collect::<Vec<_>>()
 }
-
-//34-82,33-81 -> ((34,82), (33, 81))
 
 fn main() -> io::Result<()> {
     let stdin = io::stdin();
-    let pairs = stdin.lines().map(line_to_pair);
-    let solution: i32 = pairs.map(fully_contains).sum();
-    // let solution: i32 = pairs.map(overlaps).sum();
+    let pairs: Vec<Vec<(i32,i32)>> = stdin.lines().map(|l| l.unwrap()).map(line_to_pair).collect();
 
-    println!("solution: {:?}", solution);
+    let pt_1: i32 = pairs.clone().into_iter().map(fully_contains).sum();
+    let pt_2: i32 = pairs.into_iter().map(overlaps).sum();
+    println!("pt 1: {:?}", pt_1);
+    println!("pt 2: {:?}", pt_2);
+
     Ok(())
 }
 
